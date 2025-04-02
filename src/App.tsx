ecf4d432965fcf7f30a990b0b1cec77506f4314a
퇴역군인 <freebird_han@naver.com>
2025-04-02 16:27:40 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const 내이름: string = "무리무리";
+  // 내이름 =123'
+  let 내나이: number;
+  내나이 = 345;
+  내나이 = 22;
+  function 내나이불러줘(){
+    console.log(내나이)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{내이름}</h1>
+      <h2>내나이: {내나이}</h2>
+      <button onClick={내나이불러줘}>눌리면 나이 말해주는 버튼</button>
     </>
-  )
-}
-
-export default App
+  );
+};
+export default App;
