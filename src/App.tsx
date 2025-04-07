37afedf6babe06afb657bb83977e6c200e1ab3ad
freebird920 <freebird_han@naver.com>
2025-04-07 23:26:44 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ import {
 } from "react";
 import * as PixiJs from "pixi.js";
 const App = () => {
+  // useRef()
   const canvasDivRef = useRef<HTMLDivElement>(null);
+
+  // useMemo()
   const pixiApp = useMemo(() => new PixiJs.Application(), []);
+
+  // useCallback
   const initPixi = useCallback(async () => {
     if (!canvasDivRef.current) return;
     await pixiApp.init({
@@ -16,34 +21,52 @@ const App = () => {
     });
 
     canvasDivRef.current.appendChild(pixiApp.canvas);
-    // Load the bunny texture.
-    const texture = await PixiJs.Assets.load(
-      "https://pixijs.com/assets/bunny.png"
-    );
 
-    // Create a new Sprite from an image path
-    const bunny = new PixiJs.Sprite(texture);
-
-    // Center the sprite's anchor point
-    bunny.anchor.set(0.5);
-
-    // Move the sprite to the center of the screen
-    bunny.x = pixiApp.screen.width / 2;
-    bunny.y = pixiApp.screen.height / 2;
-    // Add to stage
-    pixiApp.ticker.add((time) => {
-      bunny.rotation += 0.001 * time.deltaTime;
+    const rectOne = new PixiJs.Graphics().rect(0, 0, 200, 200).fill(0xff0000);
+    rectOne.on("pointertap", () => {
+      alert("오 눌렸읍니다?!");
     });
-    pixiApp.stage.addChild(bunny);
+    rectOne.eventMode = "dynamic";
+    const rectTwo = new PixiJs.Graphics()
+      .rect(200, 200, 200, 200)
+      .fill(0x00ff00);
+    rectTwo.on("pointertap", () => {
+      alert("오 Wkddlspdyd눌렸읍니다?!");
+    });
+    rectTwo.eventMode = "dynamic";
+    pixiApp.stage.addChild(rectOne, rectTwo);
+
+    // // Load the bunny texture.
+    // const texture = await PixiJs.Assets.load(
+    //   "https://pixijs.com/assets/bunny.png"
+    // );
+
+    // // Create a new Sprite from an image path
+    // const bunny = new PixiJs.Sprite(texture);
+
+    // // Center the sprite's anchor point
+    // bunny.anchor.set(0.5);
+
+    // // Move the sprite to the center of the screen
+    // bunny.x = pixiApp.screen.width / 2;
+    // bunny.y = pixiApp.screen.height / 2;
+    // // Add to stage
+    // pixiApp.ticker.add((time) => {
+    //   bunny.rotation += 0.001 * time.deltaTime;
+    // });
+    // pixiApp.stage.addChild(bunny);
   }, [pixiApp]);
 
+  // useLayoutEffect
   useLayoutEffect(() => {
     initPixi();
   }, [initPixi]);
+
   return (
     <>
       <h3>Pixi.js 연습하기</h3>
-      <div ref={canvasDivRef}></div>
+
+      <div className="w-[800px] h-[600px]" ref={canvasDivRef}></div>
     </>
   );
 };
